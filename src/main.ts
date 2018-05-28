557ffdd9ee0422c36f876771de00a45ac63db2b3
dmike16 <cipmiky@gmail.com>
2018-05-28 23:20:31 +0200

    fix(core): enable prod mode if webpack mode=production

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { enableProdMode } from '@angular/core';
 
 import { AppModule } from './app/app.module';
 
-if (process.env.ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   enableProdMode();
   console.info(`%c${process.env.PROJECT_NAME}`, 'font-size:13px;color:#0147A7;');
   console.info(`%cv${process.env.VERSION}`, 'font-size:11px;color:#c4342e;');
