a650afa82a5d437999d749daffecbe5106f3a384
dmike16 <cipmiky@gmail.com>
2018-06-29 17:39:34 +0200

    tool(packing): generate ssl cert with devcert

--- tools/gulp/tasks/serve-task.ts
+++ tools/gulp/tasks/serve-task.ts
@@ -1,13 +1,17 @@
 import { task } from 'gulp';
 import { webpackServe, webpackDevServe } from '@ngx-lab1100/process';
 import { WebpackServePackage } from '@ngx-lab1100/packages';
+import { certificateFor } from 'devcert';
 
 export function createServeWebpackTask(servePack: WebpackServePackage): void {
     task(`${servePack.getName()}:serve`, (cb: (err?: any) => void) => {
         const argv = servePack.getArgv({ hmr: false });
         servePack.wbo.buildConfig.hmr = argv.hmr === true;
-        // TODO: dmike16 fix webpack-serve not reloading
-        webpackDevServe(servePack.getConfig(), cb);
-        // webpackServe(servePack.getConfig(), cb);
+        certificateFor('localhost', { skipHostsFile: true }).then((certs) => {
+            servePack.wbo.buildConfig.https = certs;
+            webpackDevServe(servePack.getConfig(), cb);
+            // TODO(dmike16 ):fix webpack-serve not reloading
+            // webpackServe(servePack.getConfig(), cb);
+        }).catch((err) => cb(err));
     });
 }
